c890765b25d178f55a1e4c31ac49679dfa8dfeb3
ani <godwin@istrategytech.com>
2023-09-08 08:41:19 +0100

    add circle avatar for picture

--- app/src/main/java/com/ani/hngfirsttask/MainActivity.kt
+++ app/src/main/java/com/ani/hngfirsttask/MainActivity.kt
@@ -3,14 +3,19 @@ package com.ani.hngfirsttask
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ani.hngfirsttask.ui.theme.HngFirstTaskTheme
+import com.ani.hngfirsttask.widgets.CircleAvatar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SlackIdentity()
                 }
             }
         }
@@ -30,14 +35,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HngFirstTaskTheme {
-        Greeting("Android")
+fun SlackIdentity() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .fillMaxWidth()
+    ) {
+        CircleAvatar(imageId = R.drawable.ani, contentDescription = "fine ani", circleSize = 64)
     }
 }
